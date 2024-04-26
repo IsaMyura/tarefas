518aeb0d34726ad85419d1858ef0308fb1b52f1b
Joao Pedro Lulio <JPLulio-InfI221@AD.ETECPA.COM.BR>
2024-04-26 20:09:40 -0300

    listar tarefas

---
+++ round/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get("/",(req, res)=>{
+    res.send("Olá Mundo");
+});
+
+app.listen(PORT,()=>{
+    console.log('funfando na porta'+PORT);
+});
+
+
+
